ba2ce3b4d9f66a505d180f507f994b488dff9573
alechp <me@alechp.com>
2018-06-19 14:38:59 -0700

    Added unstated dep & wrapped Router in unstated's Provider

--- src/routes.js
+++ src/routes.js
@@ -9,36 +9,33 @@ import { Signup } from "./c/Signup";
 import { Verify } from "./c/Verify";
 import { Login } from "./c/Login";
 import { Private } from "./c/Private";
-import { Provider } from "unstated";
 
 import { Switch } from "react-router-dom";
 export default ({ childProps }) => (
-  <Provider>
-    <Switch>
-      <AppliedRoute exact path="/" component={Home} props={childProps} />
-      <UnauthenticatedRoute
-        exact
-        path="/login"
-        component={Login}
-        props={childProps}
-      />
-      <UnauthenticatedRoute
-        exact
-        path="/signup"
-        component={Signup}
-        props={childProps}
-      />
-      <UnauthenticatedRoute
-        exact
-        path="/verify"
-        component={Verify}
-        props={childProps}
-      />
-      <AuthenticatedRoute
-        path="/private"
-        component={Private}
-        props={childProps}
-      />
-    </Switch>
-  </Provider>
+  <Switch>
+    <AppliedRoute exact path="/" component={Home} props={childProps} />
+    <UnauthenticatedRoute
+      exact
+      path="/login"
+      component={Login}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
+      exact
+      path="/signup"
+      component={Signup}
+      props={childProps}
+    />
+    <UnauthenticatedRoute
+      exact
+      path="/verify"
+      component={Verify}
+      props={childProps}
+    />
+    <AuthenticatedRoute
+      path="/private"
+      component={Private}
+      props={childProps}
+    />
+  </Switch>
 );
